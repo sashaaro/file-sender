c38a441ba194ca791fd0817076fb734b067204b1
Aleksandr <sashaaro@gmail.com>
2019-10-20 11:51:29 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sashaaro/ice
+
+go 1.12
+
+require github.com/pion/ice v0.7.0
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/pion/ice"
 )
@@ -16,13 +17,24 @@ func main() {
 	checkError(err)
 	config := &ice.AgentConfig{
 		Urls: []*ice.URL{url},
+		NetworkTypes: []ice.NetworkType{
+			ice.NetworkTypeUDP4,
+			ice.NetworkTypeTCP4,
+		},
+		CandidateTypes: []ice.CandidateType{
+			ice.CandidateTypeHost,
+			ice.CandidateTypeServerReflexive,
+			ice.CandidateTypePeerReflexive,
+			ice.CandidateTypeRelay,
+		},
+		// LoggerFactory: logging.NewDefaultLoggerFactory(),
 	}
 
 	agent, err := ice.NewAgent(config)
 	checkError(err)
 	err = agent.OnCandidate(func(candidate ice.Candidate) {
 		if candidate == nil {
-
+			fmt.Printf("No candidate\n\n")
 		} else {
 			fmt.Printf("%s\n\n", candidate.String())
 		}
@@ -30,20 +42,36 @@ func main() {
 	checkError(err)
 
 	err = agent.OnConnectionStateChange(func(state ice.ConnectionState) {
-		fmt.Printf("%s\n", state.String())
+		fmt.Printf("State Change: %s\n", state.String())
 	})
 	checkError(err)
 
-
-	err = agent.GatherCandidates()
-	checkError(err)
+	/*err = agent.GatherCandidates()
+	checkError(err)*/
 
 	candidates, err := agent.GetLocalCandidates()
 	checkError(err)
 
-	fmt.Printf("%v", candidates)
+	for _, c := range candidates {
+		err = agent.AddRemoteCandidate(c)
+		checkError(err)
+	}
+
+	fmt.Printf("%v\n", candidates)
 	//err = agent.AddRemoteCandidate(candidate)
 
+	uflag, pass := agent.GetLocalUserCredentials()
+	fmt.Printf("uflag %s pass %s\n", uflag, pass)
 
-	select {}
+	checkError(err)
+
+	for {
+		conn, err := agent.Accept(context.Background(), uflag, pass)
+		checkError(err)
+
+		buffer := make([]byte, 100)
+		n, err := conn.Read(buffer)
+		checkError(err);
+		fmt.Printf("Read %v bytes", n)
+	}
 }
